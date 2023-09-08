7c1225170f6b100b65420ee34715a7d80ca901c7
Aurelien Lourot <software@lourot.dev>
2023-09-08 19:09:12 +0200

    qual: fix CI

--- example/clean-build-and-run.sh
+++ example/clean-build-and-run.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+option=$1
+
 pushd ../
 rm -rf dist/ node_modules/ package-lock.json frameright-react-image-display-control-*.tgz
 npm install
@@ -12,4 +14,7 @@ rm -rf dist/ node_modules/ package-lock.json .parcel-cache/
 mv ../frameright-react-image-display-control-*.tgz frameright-react-image-display-control.tgz
 npm install
 npm run build
-npm start
+
+if [ "$option" != "--build-only" ]; then
+  npm start
+fi
